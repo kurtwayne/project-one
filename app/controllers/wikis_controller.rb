f0f85103101d80c27e69f53f64343f074038e02b
Kurt <kurtwgardner@gmail.com>
2016-03-01 19:52:53 -0700

    added create and show views of my wikis

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -4,14 +4,29 @@ class WikisController < ApplicationController
     @wikis = Wiki.all
   end
 
-  def create
-  end
-
-  def update
+  def show
+    @wiki = Wiki.find(params[:id])
   end
 
   def new
     @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving the wiki. Please try again."
+      render :new
+    end
+  end
+
+  def update
   end
 
   def destroy
