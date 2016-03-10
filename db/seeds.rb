d038886c9b16f77fde4519af3cebe7933c4e93d6
Kurt <kurtwgardner@gmail.com>
2016-03-10 06:55:15 -0700

    Updated Seed file with Faker seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
 require 'faker'
 
-50.times do
-  Wiki.create!(
-  title: Faker::Title.title 
-  )
+5.times do
+   User.create!(
+    # name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+   )
+ end
+ users = User.all
 
+25.times do
+  Wiki.create!(
+   title: Faker::Lorem.sentence,
+   body: Faker::Lorem.paragraph
+  )
 end
+wikis = Wiki.all
+
+puts "Faker Seed Finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
